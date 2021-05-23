cbf7a0aea5df8088079314e3a858ef56fac35dbf
tudor.farcasanu <tudor.farcasanu@fep7-1.grid.pub.ro>
2021-05-23 22:43:23 +0300

    Cleaned

--- src/main.c
+++ src/main.c
@@ -1,26 +1,16 @@
 #include "list.h"
 #include "utils.h"
-// int main()
-// {
-//      FILE *f = fopen("/export/home/acs/stud/t/tudor.farcasanu/lan-party-02-checker/date/t1/d.in", READ_MODE);
-//      node *tet=makeList(f);
-//      // tet=removeDuplicates(tet);
-//      print(tet);
-// }
 
 int main(int argc, char **argv)
 {
      int vcer[5];
      FILE *cer = fopen(argv[1], READ_MODE);
      FILE *f = fopen(argv[2], READ_MODE);
-     // FILE *cer = fopen("/export/home/acs/stud/t/tudor.farcasanu/lan-party-02-checker/date/t4/c.in", READ_MODE);
-     // FILE *f = fopen("/export/home/acs/stud/t/tudor.farcasanu/lan-party-02-checker/date/t4/d.in", READ_MODE);
      fscanf(cer, "%d%d%d%d%d", &vcer[0], &vcer[1], &vcer[2], &vcer[3], &vcer[4]);
      if (vcer[0] == 1 && vcer[1] == 0)
      {
           node *tet = makeList(f);
           print(tet, argv[3]);
-          // print(tet, "/export/home/acs/stud/t/tudor.farcasanu/lan-party-02-checker/out/out3.out");
      }
      if (vcer[1] == 1 && vcer[0] == 1)
      {
@@ -28,7 +18,6 @@ int main(int argc, char **argv)
           node *tet = makeList(f);
           if (((tet->numar_echipe) & (tet->numar_echipe - 1)) == 0)
                print(tet, argv[3]);
-               // print(tet, "/export/home/acs/stud/t/tudor.farcasanu/lan-party-02-checker/out/out3.out");
           else
           {
                for (int i = tet->numar_echipe; i >= 1; i--)
@@ -42,7 +31,6 @@ int main(int argc, char **argv)
                tet = removeLast(tet, ((tet->numar_echipe) - tmp));
                tet->numar_echipe = (tet->numar_echipe) - tmp;
                print(tet, argv[3]);
-               // print(tet, "/export/home/acs/stud/t/tudor.farcasanu/lan-party-02-checker/out/out4.out");
           }
      }
 }
